1569b4b97edcc1744a2649a47fc8587e00e7daa8
jaeheonki <97086650+jaeheonki@users.noreply.github.com>
2026-04-09 13:07:39 +0900

    [level 2] Title: 기능개발, Time: 0.05 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,23 +1,23 @@
 import math
 
 def solution(progresses, speeds):
-    stack = []  #기술 배포 대기 스택
     res = []
-    today = 0
-    for i in range(len(progresses)) :
-        # days : 기능 개발까지 걸리는 일수
-        # 나눗셈의 결과가 ex. 7.3일 이렇게 나오면 8일이 걸리므로 math.ceil 사용
+    count = 0
+    max_day = 0  # 현재 배포 그룹의 최대 소요일
+
+    for i in range(len(progresses)):
         days = math.ceil((100 - progresses[i]) / speeds[i])
-        if stack and max(stack) < days : 
-            res.append(len(stack))
-            stack.clear()
-            stack.append(days)
-            
-            print(stack)
-        else :
-            stack.append(days)
-            
-    if stack :
-        res.append(len(stack))
-        
+
+        if count > 0 and max_day < days:
+            # 새로운 배포 그룹 시작
+            res.append(count)
+            count = 0
+            max_day = 0
+
+        max_day = max(max_day, days)
+        count += 1
+
+    if count > 0:
+        res.append(count)
+
     return res
