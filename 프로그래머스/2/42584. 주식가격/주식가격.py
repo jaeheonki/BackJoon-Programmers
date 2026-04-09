a350cd6c20086f229bc6c2e934374eb50882bbbd
jaeheonki <97086650+jaeheonki@users.noreply.github.com>
2026-04-09 14:06:25 +0900

    [level 2] Title: 주식가격, Time: 25.94 ms, Memory: 19 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,17 +1,18 @@
 def solution(prices):
-    res = []
-    last_index = len(prices) - 1
-    
-    for i in range(len(prices)) :
-        if i == last_index : 
-            res.append(0)
-        
-        for j in range(i + 1, len(prices)) :
-            if prices[i] > prices[j] :
-                res.append(j - i)
-                break
-            elif j == last_index :
-                res.append(last_index - i)
-    
+    n = len(prices)
+    res = [0] * n
+    stack = []  # 인덱스를 저장
+
+    for i in range(n):
+        # 현재 가격이 스택 top의 가격보다 낮으면 → 가격이 떨어진 시점
+        while stack and prices[stack[-1]] > prices[i]:
+            idx = stack.pop()
+            res[idx] = i - idx  # 버틴 시간
+        stack.append(i)
+
+    # 스택에 남은 건 끝까지 가격이 안 떨어진 경우
+    while stack:
+        idx = stack.pop()
+        res[idx] = n - 1 - idx
+
     return res
-            
